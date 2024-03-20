ded205f4bd57c80763e7fa3d62a37f7796f1c0a1
necobra <necobrane105@gmail.com>
2024-03-20 18:57:49 +0200

    Added io functions

---
+++ app/io/input.py
@@ -0,0 +1,31 @@
+
+
+def input_text_from_console():
+    """
+    Function to input text from the console.
+    Returns:
+        str: text from the console
+    """
+    pass
+
+def read_text_from_file(filename):
+    """
+    Function to read text from a file
+
+    Args:
+        filename (str): The name of the file to read from.
+    Returns:
+        str: text from the file
+    """
+    pass
+
+def read_text_from_file_pandas(filename):
+    """
+    Function to read text from a file using pandas
+
+    Args:
+        filename (str): The name of the file to read from.
+    Returns:
+        str: text from the file
+    """
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,20 @@
+
+
+def output_text_to_console(text):
+    """
+    Function to output text to the console.
+
+    Args:
+        text (str): The text to output to the console.
+    """
+    pass
+
+def write_text_to_file(text, filename):
+    """
+    Function to write text to a file
+
+    Args:
+        text (str): The text to write to the file.
+        filename (str): The name of the file to write to.
+    """
+    pass
